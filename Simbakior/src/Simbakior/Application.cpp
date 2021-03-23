228ef0fbfc0819c151030fea1f7e7931e2f8150c
melam <melam@DESKTOP-8C2SJJS>
2021-03-23 18:26:06 +0200

    application set for window event callbacks & window close event

--- Simbakior/src/Simbakior/Application.cpp
+++ Simbakior/src/Simbakior/Application.cpp
@@ -1,16 +1,18 @@
 #include "smpch.h"
 #include "Application.h"
 
-#include "Simbakior/Events/ApplicationEvent.h"
 #include "Simbakior/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Simbakior {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Simbakior::Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCalllback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Simbakior::Application::~Application()
@@ -25,5 +27,19 @@ namespace Simbakior {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispacther(e);
+		dispacther.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		SM_CORE_TRACE("{0}", e);
+
+
+	}
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
--- Simbakior/src/Simbakior/Application.h
+++ Simbakior/src/Simbakior/Application.h
@@ -3,6 +3,8 @@
 #include "Core.h"
 #include "Events/Event.h"
 #include "Window.h"
+#include "Simbakior/Events/ApplicationEvent.h"
+
 
 namespace Simbakior {
 
@@ -13,7 +15,11 @@ namespace Simbakior {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
